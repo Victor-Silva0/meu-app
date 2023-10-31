52ef987e79cad247070a0a39c2c377a6b7cbfeef
Victor Amaral Silva <victoramaral.silva@hotmail.com>
2023-10-31 09:10:36 -0300

    Add files via upload

--- src/Contador.js
+++ src/Contador.js
@@ -1,49 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './Contador.css';
 import man from './homem.png';
 import woman from './mulher.png';
 
 export default function Contador() {
-    return (
-        <div>
-  <h1 id="total">Total</h1>
-  <h1 className="total">0</h1>
-  <div className="container">
-    <div id="espaco" className="column">
-      <img src={man} />
-      <p />
-        <Buttons />
-      <p>Homens</p>
+  const [menCount, setMenCount] = useState(0);
+  const [womenCount, setWomenCount] = useState(0);
+
+  const totalCount = menCount + womenCount;
+
+  return (
+    <div>
+      <h1 id="total">Total</h1>
+      <h1 className="total">{totalCount}</h1> {}
+      <div className="container">
+        <div id="espaco" className="column">
+          <img src={man} alt="Homem" />
+          <p></p>
+          <Buttons count={menCount} setCount={setMenCount} label="Homens" />
+        </div>
+        <div className="column">
+          <img src={woman} alt="Mulher" />
+          <p></p>
+          <Buttons count={womenCount} setCount={setWomenCount} label="Mulheres" />
+        </div>
+      </div>
     </div>
-    <div className="column">
-      <img src={woman} />
-      <p />
-        <Buttons />
-      <p>Mulheres</p>
-    </div>
-  </div>
-</div>
-    );
-    
-    function Buttons() {
-        const [counter, setCounter] = useState(0) 
-    
-    function handleClick1 () { 
-      setCounter(counter + 1); 
-    } 
-    
-    function handleClick2() {
-        if (counter > 0 )
-        {
-            setCounter(counter - 1); 
-        }
+  );
+}
+
+function Buttons({ count, setCount, label }) {
+  function handleClick1() {
+    setCount(count + 1);
+  }
+
+  function handleClick2() {
+    if (count > 0) {
+      setCount(count - 1);
     }
-    return (
-        <>
-        <button onClick={handleClick1} className="homem-plus"> + </button>
-        <button onClick={handleClick2} className='homem-minus'> - </button>
-        <h1 className="pessoa">{counter}</h1>
-        </>
-    )
-    }
+  }
+
+  return (
+    <>
+      <button onClick={handleClick1} className="plus"> + </button>
+      <button onClick={handleClick2} className="minus"> - </button>
+      <h1 className="pessoa">{count}</h1>
+      <p>{label}</p>
+    </>
+  );
 }
